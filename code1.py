6bc673513bbf8960a3094ebfc8d13311fcac0c7c
vishal p <vishalfromvirali@gmail.com>
2024-12-01 01:17:55 +0530

    Add files via upload

---
+++ code1.py
@@ -0,0 +1,14 @@
+print("hello this is my first python code after 3 years")
+a =int(input("enter your 12th mark :  "))
+b=int(input("enter your 10T mark :  "))
+avg =(a/600)*100
+avg2=(b/500)*100
+if(avg>90):
+    print("you are a topper",avg)
+elif(avg>70):
+    print("good mark",avg)
+elif(avg<50):
+    print("need to improve",avg)
+else:
+    print("consontrate more on the study dont play free fire",avg)
+        
